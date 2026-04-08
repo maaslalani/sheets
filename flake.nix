6586bf5c18b50cd5180d0d342e0ec95a47e6d161
Mahyar Mirrashed <mah.mirr@gmail.com>
2026-04-08 12:11:21 -0700

    style: format flake.nix

--- flake.nix
+++ flake.nix
@@ -1,16 +1,23 @@
 {
-  description = "sheets devshell and package";
+  description = "Flake for github:maaslalani/sheets";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
       let
         pkgs = import nixpkgs { inherit system; };
-      in {
+      in
+      {
         devShells.default = pkgs.mkShell {
           name = "sheets-devshell";
 
@@ -31,7 +38,10 @@
           vendorHash = "sha256-WWtAt0+W/ewLNuNgrqrgho5emntw3rZL9JTTbNo4GsI=";
 
           subPackages = [ "." ];
-          ldflags = [ "-s" "-w" ];
+          ldflags = [
+            "-s"
+            "-w"
+          ];
 
           meta = with pkgs.lib; {
             description = "Terminal based spreadsheet tool";
@@ -44,5 +54,6 @@
           type = "app";
           program = "${self.packages.${system}.default}/bin/sheets";
         };
-      });
+      }
+    );
 }
